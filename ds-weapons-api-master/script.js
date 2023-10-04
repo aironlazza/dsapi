65e2cf134f962f5ab7438eaec8d3f3e7103249d5
aironlazza <137323731+aironlazza@users.noreply.github.com>
2023-10-04 04:41:38 -0300

    daggers e straight swords

---
+++ ds-weapons-api-master/script.js
@@ -0,0 +1,20 @@
+let hoverNome = document.getElementById("hover-nome")
+let endpoint = "https://jgalat.github.io/ds-weapons-api/"
+// function getType(weapon)
+// {
+//     return weapon.weapon_type =="greatsword"
+// }
+
+fetch(endpoint)
+.then(resposta=>resposta.json())
+.then(dados=>{
+    console.log(dados)
+    // par.innerHTML = dados[4].name
+
+    // let result = dados.filter(getType)
+    // console.log(result)
+})
+// fetch("weapons.json")
+// .then(dados =>{
+//     console.log(dados)
+// })
--- script.js
+++ script.js
@@ -1,16 +1,70 @@
-let par = document.getElementById("hover-nome")
-let endpoint = "https://jgalat.github.io/ds-weapons-api/"
-// function getType(weapon)
-// {
-//     return weapon.weapon_type =="greatsword"
-// }
+let iconArray = document.getElementsByClassName("icon-a")
 
+let hoverNome = document.getElementById("hover-nome")
+let preview = document.getElementById("preview")
+
+let nome = document.getElementById("name")
+let category = document.getElementById("weapon_category")
+let weight = document.getElementById("weight")
+let durability = document.getElementById("durability")
+let dmgPhysical = document.getElementById("dmg_physical")
+let dmgMagic = document.getElementById("dmg_magic")
+let dmgFire = document.getElementById("dmg_fire")
+let dmgLightning = document.getElementById("dmg_lightning")
+let protPhysical = document.getElementById("prot_physical")
+let protMagic = document.getElementById("prot_magic")
+let protFire = document.getElementById("prot_fire")
+let protLightning = document.getElementById("prot_lightning")
+let bonusStrength = document.getElementById("bonus_strength")
+let bonusDex = document.getElementById("bonus_dexterity")
+let bonusInt = document.getElementById("bonus_intelligence")
+let bonusFaith = document.getElementById("bonus_faith")
+let reqStr = document.getElementById("req_strength")
+let reqDex = document.getElementById("req_dexterity")
+let reqInt = document.getElementById("req_intelligence")
+let reqFaith= document.getElementById("req_faith")
+
+let endpoint = "https://jgalat.github.io/ds-weapons-api/"
 fetch(endpoint)
 .then(resposta=>resposta.json())
 .then(dados=>{
     console.log(dados)
-    // par.innerHTML = dados[4].name
+    for (var i = 0; i< iconArray.length; i++)
+    {
+      iconArray[i].addEventListener("mouseover", function(){
+        arma = dados[this.id]
+        dano = arma.damage
+        prot = arma.damage_reduction
+        bonus = arma.bonus
+        req = arma.requirements
 
-    // let result = dados.filter(getType)
-    // console.log(result)
+        hoverNome.innerHTML = arma.name
+        nome.innerHTML = dados[this.id].name
+        // .innerHTML = dados[this.id].
+        category.innerHTML = dados[this.id].weapon_type
+        weight.innerHTML = dados[this.id].weight
+        durability.innerHTML = arma.durability
+        dmgPhysical.innerHTML = arma.damage.physical
+        dmgMagic.innerHTML = dano.magic
+        dmgFire.innerHTML = dano.fire
+        dmgLightning.innerHTML = dano.lightning
+        protPhysical.innerHTML = prot.physical
+        protMagic.innerHTML = prot.magic
+        protFire.innerHTML = prot.fire
+        protLightning.innerHTML= prot.lightning
+        bonus.strength === null ? bonusStrength.innerHTML = "-" : bonusStrength.innerHTML = bonus.strength;
+        bonus.dexterity === null ? bonusDex.innerHTML = "-" : bonusDex.innerHTML = bonus.dexterity;
+        bonus.intelligence === null ? bonusInt.innerHTML = "-" : bonusInt.innerHTML = bonus.intelligence;
+        bonus.faith === null ? bonusFaith.innerHTML = "-" : bonusFaith.innerHTML = bonus.faith;
+        req.strength === null ? reqStr.innerHTML = "-" : reqStr.innerHTML = req.strength;
+        req.dexterity === null ? reqDex.innerHTML = "-" : reqDex.innerHTML = req.dexterity;
+        req.intelligence === null ? reqInt.innerHTML = "-" : reqInt.innerHTML = req.intelligence;
+        req.faith === null ? reqFaith.innerHTML = "-" : reqFaith.innerHTML = req.faith;
+
+        let foto = this.querySelector(".hover-foto")
+        preview.src = foto.src
+
+
+      }, false)
+    }
 })
